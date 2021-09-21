4db96f1c57b2487ddec8d23a9c1e06046b7e92ba
dannyedgel <edgeldan@gmail.com>
2021-09-21 12:06:37 -0500

    fixed plotting commands

--- f21/econ899/Q1/problem_sets/PS2/edgel_ps2.jl
+++ f21/econ899/Q1/problem_sets/PS2/edgel_ps2.jl
@@ -4,7 +4,7 @@ include("edgel_model_functions.jl") #import the functions that solve our growth 
 
 prim, res = Initialize() #initialize primitive and results structs
 @elapsed Solve_model(prim, res) #solve the model!
-@unpack val_func, pol_func, μ = res
+@unpack val_func, pol_func, μ, q̄ = res
 @unpack a_grid = prim
 
 ##############Make plots
@@ -12,22 +12,17 @@ prim, res = Initialize() #initialize primitive and results structs
 #stationary distribution 
 Q = sum(res.μ, dims = 1)
 Plots.plot(a_grid, transpose(Q))
+Plots.plot(a_grid, transpose(μ))
 
 #value function
-Plots.plot(k_grid, transpose(val_func), title="Value Functions",
-                label = ["S = e" "S = u"], legend=:bottomleft)
+Plots.plot(a_grid, transpose(val_func), title="Value Functions",
+                label = ["S = e" "S = u"], legend=:topleft)
 Plots.savefig("02_Value_Functions.png")
 
 #policy functions
-Plots.plot(k_grid, transpose(pol_func), title="Policy Functions",
-                label = ["Z = 1.25" "Z = 0.2"], legend=:topleft)
+Plots.plot(a_grid, transpose(pol_func), title="Policy Functions",
+                label = ["S = e" "S = u"], legend=:topleft)
 Plots.savefig("02_Policy_Functions.png")
-
-#changes in policy function
-pol_func_δ = transpose(copy(pol_func)[:,2:end].-copy(pol_func)[:,1:(end-1)])
-Plots.plot(k_grid[1:(end-1)], pol_func_δ, title="Policy Functions Changes",
-                label = ["Z = 1.25" "Z = 0.2"], legend=:bottomright)
-Plots.savefig("02_Policy_Functions_Changes.png")
 
 println("All done!")
 ################################
