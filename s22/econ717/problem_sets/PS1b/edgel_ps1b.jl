8f031a8c734b7409d8eb382e50dfa6d8a955673e
dannyedgel <edgeldan@gmail.com>
2022-04-19 14:18:50 -0500

    ps1b finished

--- s22/econ717/problem_sets/PS1b/edgel_ps1b.jl
+++ s22/econ717/problem_sets/PS1b/edgel_ps1b.jl
@@ -3,7 +3,7 @@
     of the second quarter of Econ 717
 
     Date created:  08 Apr 2022
-    Last modified: 13 Apr 2022
+    Last modified: 19 Apr 2022
     Author: Danny Edgel (edgel@wisc.edu)
 ==#
 
@@ -15,6 +15,9 @@ using Optim, Random, Plots, Printf
 
 # declare user-written functions 
 include("./ps1b_functions.jl")
+
+# choose number of simulations to run
+n = 10000
 
 ### Questions
 
@@ -34,72 +37,107 @@ prim, θ = Initialize()
 # space (parameter-by-parameter) to find θ such that ~60% of 
 # simulated agents choose 1
 
-θ₀ = θ
+θ₀ = copy(θ)
 
 replFn = (x, i, θ) -> [j ≠ i ? θ[j] : x for j in 1:length(θ)]
+range = 0.1:0.005:10
+iter = 0
+while (abs(mean(SimulateData(θ₀, n; seed=3609).j .== 1) - 0.6) > 0.005) & (iter < 5)
+    for i ∈ 3:(length(θ)-1)
+        ygrid = [OccChoiceObj(replFn(x, i, θ₀)) for x in range]
+        θ₀[i] = (range)[findmin(ygrid)[2]]
+    end
 
-for i ∈ 3:(length(θ)-1)
-    ygrid = [OccChoiceObj(replFn(x, i, θ₀)) for x in 0.1:0.1:50]
-    θ₀[i] = (0.1:0.1:50)[findmin(ygrid)[2]]
+    ygrid = [OccChoiceObj(replFn(x, length(θ₀), θ₀)) for x in -0.999:0.001:0.999]
+    θ₀[length(θ₀)] = (-0.999:0.001:0.999)[findmin(ygrid)[2]]
+
+    iter += 1
+    print("\nIteration " * string(iter) * ", err = " *
+          string(abs(mean(SimulateData(θ₀, n; seed=3609).j .== 1) - 0.6)))
 end
 
-ygrid = [OccChoiceObj(replFn(x, length(θ₀), θ₀)) for x in 0.001:0.001:0.999]
-θ₀[length(θ₀)] = (0.001:0.001:0.999)[findmin(ygrid)[2]]
-
 OccChoiceObj(θ₀)
-mean(SimulateData(θ₀, 1000).j .== 1)
+mean(SimulateData(θ₀, n; seed=3609).j .== 1)
 
+str = @sprintf "\$\\theta=(1, 1, %.2f, %.2f, %.2f, %.2f, %.2f)\$ results in \$%.2f\$\\%% of simulated agents choosing 1 for the chosen randomization seed.
+" θ₀[3] θ₀[4] θ₀[5] θ₀[6] θ₀[7] mean(SimulateData(θ₀, n; seed=3609).j .== 1) * 100
 
+open("c.tex", "w") do io
+    write(io, str)
+end
 
 ## e) Estimate μ₁ and ρ from the simulated data, holding other params
 ##   fixed at their "true" values
 
 # generate a data set with the true parameters
-dat = SimulateData(θ₀, 1000)
+dat = SimulateData(θ₀, n)
 
 SMMObjFun(θ, dat)
 
 # define secondary objective function that holds other parameters fixed
-obj = x -> SMMObjFun([θ₀[1], θ₀[2], x[1], θ₀[4], θ₀[5], θ₀[6], x[2]], dat; N=1000, k=2)
+obj = x -> SMMObjFun([θ₀[1], θ₀[2], x[1], θ₀[4], θ₀[5], θ₀[6], x[2]], dat;
+    N=Int(n), m=5)
 
 # derive the parameter estimate
-x̂ = Optim.optimize(obj, [0.01, 0.0], [Inf, 0.99], [0.5, 0.5], Fminbox()).minimizer
+x̂ = Optim.optimize(obj, [0.01, -0.99], [Inf, 0.99], [θ[3], θ[7]], Fminbox(LBFGS())).minimizer
 
-θ = [θ₀[1], θ₀[2], x̂[1], θ₀[4], θ₀[5], θ₀[6], x̂[2]]
+str = @sprintf "\$(\\hat{\\mu_1}, \\hat{\\rho})=(%.2f, %.2f)\$.
+" x̂[1] x̂[2]
+
+open("e.tex", "w") do io
+    write(io, str)
+end
 
 ## f) Create some figures that show that x̂ identifies the true parameters
 
 μgrid = 0.001:0.001:0.99
-ρgrid = 0:0.0005:0.9995
-ygridμ = [obj([x, x̂[2]]) for x in μgrid]
-ygridρ = [obj([x̂[1], x]) for x in ρgrid]
+ρgrid = -0.1:0.0005:0.5
+#ygridμ = [obj([x, x̂[2]]) for x in μgrid]
+#ygridρ = [obj([x̂[1], x]) for x in ρgrid]
+ygridμ = [obj([x, θ₀[7]]) for x in μgrid]
+ygridρ = [obj([θ₀[3], x]) for x in ρgrid]
 
-plot(μgrid, ygridμ)
-plot(ρgrid, ygridρ)
-plot(ρgrid[ρgrid.<=0.9], ygridρ[ρgrid.<=0.9])
+plot(μgrid, ygridμ, labels="SMM Obj Fun", title="\\mu_1 identification")
+plot!([θ₀[3]], seriestype="vline", labels="True")
+plot!([x̂[1]], seriestype="vline", labels="Estimate", linestyle=:dash)
+savefig("f1.png")
+
+plot(ρgrid, ygridρ, labels="SMM Obj Fun", title="\\rho identification")
+plot!([θ₀[7]], seriestype="vline", labels="True")
+plot!([x̂[2]], seriestype="vline", labels="Estimate", linestyle=:dash)
+savefig("f2.png")
+
+
+θ = [θ₀[1], θ₀[2], x̂[1], θ₀[4], θ₀[5], θ₀[6], x̂[2]]
+
 
 ## g) report average number of agents who choose 1, and the
 ##    average and standard deviation of wages in each occupation
 
-# simulate data with the estimated parameters
-sim = SimulateData(θ, 1000)
+# simulate data with the estimated parameters (table defined in question h)
+sim = SimulateData(θ, n; seed=1326)
 
-str = @sprintf "\\begin{tabular}{r|cc} 
-                    & Data & Model \\\\\\hline
-    %% Choosing j=1 & %0.2f & %0.2f \\\\
-    \$\\E{W|j=1}\$   & %0.2f & %0.2f \\\\
-    \$\\E{W|j=2}\$   & %0.2f & %0.2f \\\\
-    \$Var(W|j=1)\$   & %0.2f & %0.2f \\\\
-    \$Var(W|j=2)\$   & %0.2f & %0.2f \\\\
-    \end{tabular}
-" mean(dat.j .== 1) mean(sim.j .== 1) mean(dat.W[dat.j.==1]) mean(sim.W[sim.j.==1]) mean(dat.W[dat.j.==2]) mean(sim.W[sim.j.==2]) var(dat.W[dat.j.==1]) var(sim.W[sim.j.==1]) var(dat.W[dat.j.==2]) var(sim.W[sim.j.==2])
 
+## h) find w̲ such that 70% of agents choose occupation 1
+obj = x -> OccChoiceObj(θ; p=0.7, w̲=x, N=Int(n / 100), seed=214)
+ŵ̲ = (0.0:0.001:2.0)[findmin([obj(x) for x in 0.0:0.001:2.0])[2]]
+
+str = @sprintf "\$%.3f\$." ŵ̲
+open("h.tex", "w") do io; write(io, str); end
+
+
+sim2 = SimulateData(θ, n; seed=1326, w̲=ŵ̲)
+
+str = @sprintf "\\begin{tabular}{r|ccc} 
+                        & Data  & Model & \$\\underline{\\hat{W}}_1\$ \\\\\\hline
+    \\%% Choosing j=1   & %0.3f & %0.3f & %0.3f                       \\\\
+    \$\\E{W|j=1}\$      & %0.3f & %0.3f & %0.3f                       \\\\
+    \$\\E{W|j=2}\$      & %0.3f & %0.3f & %0.3f                       \\\\
+    \$Var(W|j=1)\$      & %0.3f & %0.3f & %0.3f                       \\\\
+    \$Var(W|j=2)\$      & %0.3f & %0.3f & %0.3f                       \\\\
+    \\end{tabular}
+" mean(dat.j .== 1) mean(sim.j .== 1) mean(sim2.j .== 1) mean(dat.W[dat.j.==1]) mean(sim.W[sim.j.==1]) mean(sim2.W[sim2.j.==1]) mean(dat.W[dat.j.==2]) mean(sim.W[sim.j.==2]) mean(sim2.W[sim2.j.==2]) var(dat.W[dat.j.==1]) var(sim.W[sim.j.==1]) var(sim2.W[sim2.j.==1]) var(dat.W[dat.j.==2]) var(sim.W[sim.j.==2]) var(sim2.W[sim2.j.==2])
 
 open("table1.tex", "w") do io
     write(io, str)
 end
-
-## h) find w̲ such that 70% of agents choose occupation 1
-
-obj = x -> OccChoiceObj(θ; p=0.7, w̲ = x)
-ŵ̲ = (0.0:0.001:2.0)[findmin([obj(x) for x in 0.0:0.001:2.0])[2]]
--- s22/econ717/problem_sets/PS1b/ps1b_functions.jl
+++ s22/econ717/problem_sets/PS1b/ps1b_functions.jl
@@ -24,6 +24,21 @@ using Parameters, Random, DataFrames, Distributions, LinearAlgebra, StatsBase
 
 end # Primitives struct 
 
+### Define a function that initializes the primitives
+function Initialize(; θ::Vector{Float64} = [1, 1, 0.5, 0.5, 0.25, 0.25, 0.4])
+
+    # Unpack the parameters of the parameter vector
+    π₁, π₂, μ₁, μ₂, σ₁, σ₂, ρ = θ
+
+    # Initialize the primitives struct
+    prim = Primitives(π₁=π₁, π₂=π₂, μ₁=μ₁, μ₂=μ₂, σ₁=σ₁, σ₂=σ₂,
+        ρ=ρ, Σ=[σ₁^2 ρ*σ₁*σ₂; ρ*σ₁*σ₂ σ₂^2])
+
+    # Return the primitives struct, parameter vector, and data
+    return prim, θ
+
+end # Initialize()
+
 ### Define a function that generates data, given primitives
 function SimulateData(θ::Vector{Float64}, N::Int64; seed::Int64=115,
     w̲::Float64=-Inf)
@@ -45,41 +60,26 @@ function SimulateData(θ::Vector{Float64}, N::Int64; seed::Int64=115,
 
 end # SimulateData()
 
-### Define a function that initializes the primitives
-function Initialize(; θ::Vector{Float64} = [1, 1, 0.5, 0.5, 0.25, 0.25, 0.4])
-
-    # Unpack the parameters of the parameter vector
-    π₁, π₂, μ₁, μ₂, σ₁, σ₂, ρ = θ
-
-    # Initialize the primitives struct
-    prim = Primitives(π₁=π₁, π₂=π₂, μ₁=μ₁, μ₂=μ₂, σ₁=σ₁, σ₂=σ₂,
-        ρ=ρ, Σ=[σ₁^2 ρ*σ₁*σ₂; ρ*σ₁*σ₂ σ₂^2])
-
-    # Return the primitives struct, parameter vector, and data
-    return prim, θ
-
-end # Initialize()
-
 ### Define the objective function for finding a parameter
 ### vector that results in p% of agents choosing 1
 function OccChoiceObj(θ::Vector{Float64}; p::Float64 = 0.6, 
-    N::Int64 = 1000, w̲::Float64 = -Inf)
+    N::Int64 = 1000, w̲::Float64 = -Inf, seed::Int64 = 3609)
 
     # return the squard difference between p and p̂
-    return (p - mean(SimulateData(θ, N; w̲ = w̲).j .== 1))^2
+    return (p - mean(SimulateData(θ, N; w̲ = w̲, seed = seed).j .== 1))^2
 
 end # OccChoiceObj()
 
 ### Define the SMM objective function for finding a parameter
 function SMMObjFun(θ::Vector{Float64}, data::DataFrame; 
-    N::Int64 = 1000, k::Int64 = length(θ),
-    W::Matrix{Float64} = 1.0*Matrix(I, k, k))
+    N::Int64 = 1000, m::Int64 = length(θ), seed::Int64 = 115,
+    W::Matrix{Float64} = 1.0*Matrix(I, m, m)) 
 
     # impose boundary conditions on k 
-    @assert (k > 0 & k ≤ length(θ)) "k must be a positive integer no greater than the length of the parameter vector"
+    @assert (m > 0) "m must be a positive integer"
 
     # simulate data
-    sim = SimulateData(θ, N)
+    sim = SimulateData(θ, N; seed = seed)
 
     # calculate simulated moments
     gθ = [
@@ -103,8 +103,8 @@ function SMMObjFun(θ::Vector{Float64}, data::DataFrame;
         skewness(data.W[data.j .== 2])
     ]
 
-    # subset to the first k moments
-    gθ, ĝ = gθ[1:k], ĝ[1:k]
+    # subset to the first m moments
+    gθ, ĝ = gθ[1:m], ĝ[1:m]
 
     # return the objective function
     return (gθ - ĝ)'*W*(gθ - ĝ)
